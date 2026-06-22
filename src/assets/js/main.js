7583a0e9a7fd69ab1d7eef0605f30f8d561917b0
philippedward <amelie.rakowiecki@studentfr.ch>
2026-06-22 14:46:09 +0200

    portfolio fontonne lets goooo

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -6,6 +6,10 @@ const modal = document.querySelector(".porfolio-content-modal");
 
 document.querySelectorAll(".porfolio-open-modal").forEach(function (card) {
   card.addEventListener("click", function () {
+    const popupId = card.parentElement.getAttribute("data-portfolio-popup");
+    document
+      .getElementById(popupId)
+      .classList.remove("portfolio-selector-hidden");
     modal.classList.remove("portfolio-hidden-modal");
   });
 });
@@ -14,4 +18,10 @@ document
   .querySelector(".portfolio-close-modal")
   .addEventListener("click", function () {
     modal.classList.add("portfolio-hidden-modal");
+
+    document
+      .querySelectorAll(".portfolio-body-modal")
+      .forEach(function (contentModal) {
+        contentModal.classList.add("portfolio-selector-hidden");
+      });
   });
