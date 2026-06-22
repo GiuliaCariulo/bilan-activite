a04cf3fcd09023843c70492b332403c824922177
philippedward <amelie.rakowiecki@studentfr.ch>
2026-06-22 09:12:53 +0200

    probleme non resolut

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,17 @@
-console.log("test");
+// ============================================================
+// section portfolio : pop up
+// ============================================================
+
+const modal = document.querySelector(".porfolio-content-modal");
+
+document.querySelectorAll(".porfolio-open-modal").forEach(function (card) {
+  card.addEventListener("click", function () {
+    modal.classList.remove("portfolio-hidden-modal");
+  });
+});
+
+document
+  .querySelector(".portfolio-close-modal")
+  .addEventListener("click", function () {
+    modal.classList.add("portfolio-hidden-modal");
+  });
