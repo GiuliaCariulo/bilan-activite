149871648d93705788c7352192b5f1c6d5999574
philippedward <amelie.rakowiecki@studentfr.ch>
2026-06-23 17:11:22 +0200

    footer mobile

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -52,7 +52,7 @@ document.addEventListener("DOMContentLoaded", () => {
         endTrigger: ".footer-contact-copyright",
         end: "bottom bottom",
         scrub: true,
-        markers: false,
+        markers: true,
       },
     });
   }
