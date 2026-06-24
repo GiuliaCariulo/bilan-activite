53ba1bacfe863f5ac9d4d7bbb78ccc8658739269
philippedward <amelie.rakowiecki@studentfr.ch>
2026-06-24 11:54:28 +0200

    footer maybe

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -58,21 +58,21 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 
-  const isMobile = window.innerWidth < 768;
-  const spacing = 160;
+  // const isMobile = window.innerWidth < 768;
+  // const spacing = 160;
 
-  gsap.to(lines.slice(1), {
-    y: (i) => (i + 1) * spacing,
-    ease: "none",
-    scrollTrigger: {
-      trigger: ".footer-animation",
-      start: "top bottom",
-      endTrigger: isMobile ? ".footer-contact" : ".footer-contact-copyright",
-      end: "bottom bottom",
-      scrub: true,
-      markers: false,
-    },
-  });
+  // gsap.to(lines.slice(1), {
+  //   y: (i) => (i + 1) * spacing,
+  //   ease: "none",
+  //   scrollTrigger: {
+  //     trigger: ".footer-animation",
+  //     start: "top bottom",
+  //     endTrigger: isMobile ? ".footer-contact" : ".footer-contact-copyright",
+  //     end: "bottom bottom",
+  //     scrub: true,
+  //     markers: false,
+  //   },
+  // });
 
   // ============================================================
   // hero.js — Gestion de la grille héro et des expansions
