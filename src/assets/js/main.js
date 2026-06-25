7acc5169597535abbaf7f1522d834c911eb8fc70
philippedward <amelie.rakowiecki@studentfr.ch>
2026-06-25 16:33:25 +0200

    footer correction

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -58,22 +58,6 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 
-  // const isMobile = window.innerWidth < 768;
-  // const spacing = 38;
-
-  // gsap.to(lines.slice(1), {
-  //   y: (i) => (i + 1) * spacing,
-  //   ease: "none",
-  //   scrollTrigger: {
-  //     trigger: ".footer-animation",
-  //     start: "top bottom",
-  //     endTrigger: ".footer-contact",
-  //     end: "bottom bottom",
-  //     scrub: true,
-  //     markers: true,
-  //   },
-  // });
-
   // ============================================================
   // hero.js — Gestion de la grille héro et des expansions
   // ============================================================
