015e6e66875ef074356be5f1791d8452097e4d2a
philippedward <amelie.rakowiecki@studentfr.ch>
2026-06-29 09:15:15 +0200

    bio graphie popup

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -39,6 +39,41 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
   }
+  // ============================================================
+  // section biography : pop up
+  // ============================================================
+
+  document.querySelectorAll(".biography-open-modal").forEach((card) => {
+    card.addEventListener("click", () => {
+      const pos = card.getAttribute("data-member-pos");
+      const modalBio = document.querySelector(
+        `.biography-content-modal[data-index="${pos - 1}"]`,
+      );
+      if (!modalBio) return;
+
+      modalBio.classList.remove("biography-hidden-modal");
+      document.body.style.overflow = "hidden";
+    });
+  });
+
+  document.querySelectorAll(".biography-close-modal").forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      e.stopPropagation();
+      btn
+        .closest(".biography-content-modal")
+        .classList.add("biography-hidden-modal");
+      document.body.style.overflow = "";
+    });
+  });
+
+  document.querySelectorAll(".biography-content-modal").forEach((modal) => {
+    modal.addEventListener("click", (e) => {
+      if (e.target === modal) {
+        modal.classList.add("biography-hidden-modal");
+        document.body.style.overflow = "";
+      }
+    });
+  });
 
   // ============================================================
   // section footer : scrolling texts
