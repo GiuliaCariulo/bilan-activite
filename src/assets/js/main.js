d89bc1335ada08d10181903ba37fcaee8f7a3e68
camwizardd <camille.magnin@studentfr.ch>
2026-06-30 11:39:00 +0200

    button scrolltop added

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -453,6 +453,31 @@ document.addEventListener("DOMContentLoaded", () => {
     // also onLeave, onEnterBack, and onLeaveBack
     // also most normal ScrollTrigger values like start, end, etc.
   });
+
+  // Get the button:
+  let mybutton = document.getElementById("myBtn");
+
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function () {
+    scrollFunction();
+  };
+
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+
+  // When the user clicks on the button, scroll to the top of the document
+  mybutton.addEventListener("click", () => {
+    document.body.scrollTop = 0; // Safari
+    document.documentElement.scrollTop = 0; // Chrome, Firefox, IE, Opera
+  });
 });
 // ==========================================================================
 // loader page : loader active
