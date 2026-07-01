40ae263d10036d1a9adfa1e15e119b53fd8b049c
philippedward <amelie.rakowiecki@studentfr.ch>
2026-07-01 10:42:07 +0200

    js footer

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -104,11 +104,11 @@ document.addEventListener("DOMContentLoaded", () => {
       ease: "none",
       scrollTrigger: {
         trigger: isMobile ? ".footer-title" : ".footer-contact",
-        start: isMobile ? "top 90%" : "-20% bottom",
+        start: isMobile ? "top 85%" : "2% bottom",
         endTrigger: isMobile ? ".footer-contact" : ".footer-contact-copyright",
-        end: isMobile ? "top 100%" : "55% bottom",
+        end: isMobile ? "top 100%" : "85% bottom",
         scrub: true,
-        markers: false,
+        markers: true,
       },
     });
   }
